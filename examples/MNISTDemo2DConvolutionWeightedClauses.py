97d0a415c4b5a11b9095907ecb33062f1bd92d30
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-04-07 19:29:46 +0200

    Update

--- examples/MNISTDemo2DConvolutionWeightedClauses.py
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -17,6 +17,12 @@ epochs = 250
 batches = 10
 
 patch_size = 10
+image_size = 28
+
+number_of_x_pos_features = image_size - patch_size
+number_of_y_pos_features = image_size - patch_size
+number_of_content_features = patch_size*patch_size
+number_of_features = number_of_x_pos_features + number_of_y_pos_features + number_of_content_features
 
 (X_train, Y_train), (X_test, Y_test) = mnist.load_data()
 
@@ -43,5 +49,26 @@ for e in range(ensembles):
 		print("%d %d %.2f %.2f %.2f %.2f" % (e, i, result_train, result_test, stop_training-start_training, stop_testing-start_testing), file=f)
 		f.flush()
 
-	number_of_features = tm.number_of_features
+		# Example of getting a single clause (clause 0 of class 0)
+		class_id = 0
+		clause = 0
+
+		# If patch mask_1 contains 1, the correponding image pixel must also be 1.
+		mask_1 = np.zeros((patch_size, patch_size)).astype(np.uint8)
+		for patch_y in range(patch_size):
+			for patch_x in range(patch_size):
+				feature = number_of_x_pos_features + number_of_y_pos_features + patch_y * patch_size + patch_x
+				if tm.ta_action(class_id, clause, feature) == 1:
+					mask_1[patch_x, patch_y] = 1
+
+		# If patch mask_0 contains 1, the correponding image pixel must also be 0 (negated features)
+		mask_0 = np.zeros((patch_size, patch_size)).astype(np.uint8)
+		for patch_y in range(patch_size):
+			for patch_x in range(patch_size):
+				feature = number_of_features + number_of_x_pos_features + number_of_y_pos_features + patch_y * patch_size + patch_x
+				if tm.ta_action(class_id, clause, feature) == 1:
+					mask_0[patch_x, patch_y] = 1
+
+		print(mask_1)
+		print(mask_0)
 f.close()
