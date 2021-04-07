481f7d5711982df76ed14070f32e7a6fbcf0fc2e
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-04-07 18:37:33 +0200

    Update

--- examples/MNISTDemo2DConvolutionWeightedClauses.py
+++ examples/MNISTDemo2DConvolutionWeightedClauses.py
@@ -23,19 +23,15 @@ patch_size = 10
 X_train = np.where(X_train >= 75, 1, 0) 
 X_test = np.where(X_test >= 75, 1, 0)
 
-batch_size= X_train.shape[0]//batches
-
 f = open("mnist_%.1f_%d_%d_%d.txt" % (s, int(factor*2000), T,  patch_size), "w+")
 
 for e in range(ensembles):
 	tm = MultiClassConvolutionalTsetlinMachine2D(int(factor*2000), T, s, (patch_size, patch_size), clause_drop_p = 0.1, feature_drop_p = 0.1, number_of_gpus = 16)
 
 	for i in range(epochs):
-		for b in range(batches):
-			start_training = time()
-			tm.fit(X_train[b*batch_size:(b+1)*batch_size], Y_train[b*batch_size:(b+1)*batch_size], epochs=1, incremental=True)
-			stop_training = time()
-			print("\tBatch #%d %.2f" % (b, stop_training-start_training))
+		start_training = time()
+		tm.fit(X_train, Y_train, epochs=1, incremental=True)
+		stop_training = time()
 
 		start_testing = time()
 		result_test = 100*(tm.predict(X_test) == Y_test).mean()
@@ -46,4 +42,6 @@ for e in range(ensembles):
 		print("%d %d %.2f %.2f %.2f %.2f" % (e, i, result_train, result_test, stop_training-start_training, stop_testing-start_testing))
 		print("%d %d %.2f %.2f %.2f %.2f" % (e, i, result_train, result_test, stop_training-start_training, stop_testing-start_testing), file=f)
 		f.flush()
+
+	number_of_features = tm.number_of_features
 f.close()
