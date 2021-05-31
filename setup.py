e1c85cc3bfaaf23cbddfcda1a3aa8f8bf8436bfd
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-05-31 08:57:25 +0200

    Name change

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import *
 
 setup(
-   name='PyTsetlinMachineCUDA',
+   name='tsetlinmachinecuda',
    version='0.1.8',
    author='Ole-Christoffer Granmo',
    author_email='ole.granmo@uia.no',
-   url='https://github.com/cair/pyTsetlinMachineCUDA/',
+   url='https://github.com/cair/tsetlinmachinecuda/',
    license='MIT',
    description='Massively Parallel and Asynchronous Architecture for Logic-based AI.',
    long_description='Massively Parallel and Asynchronous Architecture for Logic-based AI. Implements the Multiclass Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and multiple layers.',
