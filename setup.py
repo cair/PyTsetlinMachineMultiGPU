fd41943ba0eb3a9f5ffdd5f70183f3cdd64b5bfe
Ole-Christoffer Granmo <ole.granmo@uia.no>
2021-05-31 09:03:11 +0200

    Name change

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
    description='Massively Parallel and Asynchronous Architecture for Logic-based AI.',
    long_description='Massively Parallel and Asynchronous Architecture for Logic-based AI. Implements the Multiclass Tsetlin Machine, Convolutional Tsetlin Machine, Regression Tsetlin Machine, and Weighted Tsetlin Machine, with support for continuous features and multiple layers.',
    keywords ='pattern-recognition cuda machine-learning interpretable-machine-learning rule-based-machine-learning propositional-logic tsetlin-machine regression convolution classification multi-layer',
-   packages=['PyTsetlinMachineCUDA']
+   packages=['tsetlinmachinecuda']
 )
